b1be47f44d04962131c3832f538e817c18a95ee4
Mikita Asotski <masotski@exadel.com>
2018-08-15 11:33:15 +0300

    add click-riderect to <input>

--- fileUpload/js/main.js
+++ fileUpload/js/main.js
@@ -1,1 +1,6 @@
 "use strict";
+window.onload = function() {
+  document.getElementById('uploaderClicker').addEventListener('click', () => {
+    document.getElementById('uploader').click();
+  }, false);
+};
